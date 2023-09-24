f42cc229125d3c1e5c90b4600119dc32a17b4fd4
Lazytitan25 <144724420+Lazytitan25@users.noreply.github.com>
2023-09-24 15:01:10 -0400

    Update myArrayList.java

--- myArrayList.java
+++ myArrayList.java
@@ -1,24 +1,42 @@
 import java.util.ArrayList;
 public class myArrayList {
 	
-	ArrayList<Fraction> list = new ArrayList<Fraction>(50);
-	
-	public static int indexOf(Fraction input) {
-		for (int i = 0; i < list.size(); ++i) {
-			if (list.get(i).compare(input) == 0) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	public void add  (int index, Fraction input)
+	int size;
+    ArrayList<Fraction> list;
+    myArrayList(int size)
+    {
+        list = new ArrayList<Fraction>(size);
+    }
     
-	{
-   		list.indexOf(i) = Fraction.input;
     
-	}
-	public static Fraction remove(int index) {
-		
-		return new Fraction(0,0);
-	}
+    
+    public int indexOf(Fraction input) {
+        for (int i = 0; i < list.size(); ++i) {
+            if (list.get(i).compare(input) == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public void add (int index, Fraction input)
+    
+    {
+        Fraction next = list.get(index);
+        Fraction oneAfter;
+        Fraction last = list.get(size  -1);
+        list .set(index, input);
+        
+        for(int i = index + 1; i < list.size(); i++)
+        {
+            oneAfter = list.get(i);
+            list.set(i, next);
+            next = oneAfter;
+        }
+        list.add(last); 
+        size = list.size(); 
+        
+    
+    
+    }
+
 }
