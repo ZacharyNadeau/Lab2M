8255f7b1dfbe3a993f5fcacb601a26695a9a8238
Lazytitan25 <144724420+Lazytitan25@users.noreply.github.com>
2023-09-24 15:02:57 -0400

    Update myArrayListadd.java

--- myArrayListadd.java
+++ myArrayListadd.java
@@ -1,9 +1,22 @@
 import java.util.ArrayList;
 
-public void add  (int index, Fraction input)
+public void add (int index, Fraction input)
     
-{
-    list.indexOf(i) = Fraction.input;
+    {
+        Fraction next = list.get(index);
+        Fraction oneAfter;
+        Fraction last = list.get(size  -1);
+        list .set(index, input);
+        
+        for(int i = index + 1; i < list.size(); i++)
+        {
+            oneAfter = list.get(i);
+            list.set(i, next);
+            next = oneAfter;
+        }
+        list.add(last); 
+        size = list.size(); 
+        
     
-}
-
+    
+    }
